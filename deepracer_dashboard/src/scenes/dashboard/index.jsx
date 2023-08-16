fba2d222fcc20720def9f9ea3730b609cfa62e10
Rishi <rishipatel3012@gmail.com>
2023-08-17 00:34:07 +1000

    made changes to the dashboard and styling changes

--- deepracer_dashboard/src/scenes/dashboard/index.jsx
+++ deepracer_dashboard/src/scenes/dashboard/index.jsx
@@ -1,16 +1,67 @@
-import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { Box, Button, IconButton, Typography, colors, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import { mockTransactions } from "../../data/mockData";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-import EmailIcon from "@mui/icons-material/Email";
-import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import TrafficIcon from "@mui/icons-material/Traffic";
+import TimerRoundedIcon from '@mui/icons-material/TimerRounded';
+import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
 import Header from "../../components/Header";
-import LineChart from "../../components/LineChart";
-import BarChart from "../../components/BarChart";
 import StatBox from "../../components/StatBox";
-import ProgressCircle from "../../components/ProgressCircle";
+import Track from "../../assets/track.png"
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#FFFFF",
+    color: colors.deepOrange[300],
+    fontSize: 20,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 20,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme, index }) => ({
+  // For gold, silver, and bronze rows
+  '&:nth-child(1)': {
+    backgroundColor: '#D4AF37', // Gold
+  },
+  '&:nth-child(2)': {
+    backgroundColor: 'silver', // Silver
+  },
+  '&:nth-child(3)': {
+    backgroundColor: '#cd7f32', // Bronze
+  },
+  // For other rows
+  '&:not(:nth-child(1)):not(:nth-child(2)):not(:nth-child(3))': {
+    backgroundColor: "#7e57c2",
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat) {
+  return { name, calories, fat,};
+}
+
+const rows = [
+  createData(1, "RMIT SPEEDSTER", "0:09:09"),
+  createData(2, "MUI RACER", "0:10:10"),
+  createData(3, "MODEL REDBULL", "0:11:10"),
+  createData(4, "MODEL FERRARI", "0:11:17"),
+  createData(5, "RACER", "0:12:10"),
+];
 
 const Dashboard = () => {
   const theme = useTheme();
@@ -25,8 +76,8 @@ const Dashboard = () => {
         <Box>
           <Button
             sx={{
-              backgroundColor: colors.blueAccent[700],
-              color: colors.grey[100],
+              backgroundColor: "#FFA500",
+              color: colors.purpleAccent[900],
               fontSize: "14px",
               fontWeight: "bold",
               padding: "10px 20px",
@@ -48,87 +99,151 @@ const Dashboard = () => {
         {/* ROW 1 */}
         <Box
           gridColumn="span 3"
-          backgroundColor={colors.orangeAccent[600]}
+          backgroundColor={"#FFA500"}
           display="flex"
-          alignItems="center"
-          justifyContent="center"
+          borderRadius="5px"
         >
-          <StatBox
-            title="12,361"
-            subtitle="Emails Sent"
+        <Box
+          display="block"
+          justifyContent="flex-start"
+        >
+          <BadgeRoundedIcon
+            sx={{ color: colors.purpleAccent[600], fontSize: "26px", marginLeft:"30px", marginTop:"30px"}}
+          />
+          <Typography 
+            variant="h3" 
+            color={colors.purpleAccent[800]}
+            align="left"
+            margin={30+"px"}
+            marginBottom={0+"px"}
+            marginTop={0+"px"}
+            fontWeight="bold"
+          >
+            RMIT SPEEDSTER
+          </Typography>
+          <Typography
+            marginLeft={30+"px"}
+            fontSize={16}
+            >
+            Model Name
+          </Typography>
+        </Box>
+          {/* <StatBox
+            title="Speedy RMIT"
+            subtitle="Model Name"
             progress="0.75"
             increase="+14%"
             style={{color: colors.orangeAccent[500] }}
             icon={
-              <EmailIcon
-                sx={{ color: colors.purpleAccent[600], fontSize: "26px" }}
-              />
+
             }
-          />
+          /> */}
         </Box>
         <Box
           gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
+          backgroundColor={"#FFA500"}
           display="flex"
-          alignItems="center"
-          justifyContent="center"
+          borderRadius="5px"
         >
-          <StatBox
-            title="431,225"
-            subtitle="Sales Obtained"
-            progress="0.50"
-            increase="+21%"
-            icon={
-              <PointOfSaleIcon
-                sx={{ color: colors.purpleAccent[600], fontSize: "26px" }}
-              />
-            }
+        <Box
+          display="block"
+          justifyContent="flex-start"
+        >
+          <TimerRoundedIcon
+            sx={{ color: colors.purpleAccent[600], fontSize: "26px", marginLeft:"30px", marginTop:"30px"}}
           />
+          <Typography 
+            variant="h3" 
+            color={colors.purpleAccent[800]}
+            align="left"
+            margin={30+"px"}
+            marginBottom={0+"px"}
+            marginTop={0+"px"}
+            fontWeight="bold"
+          >
+            1:23:00
+          </Typography>
+          <Typography
+            marginLeft={30+"px"}
+            fontSize={16}
+            >
+            Fatest Time
+          </Typography>
+        </Box>
         </Box>
         <Box
           gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
+          backgroundColor={"#FFA500"}
           display="flex"
-          alignItems="center"
-          justifyContent="center"
+          borderRadius="5px"
         >
-          <StatBox
-            title="32,441"
-            subtitle="New Clients"
-            progress="0.30"
-            increase="+5%"
-            icon={
-              <PersonAddIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
+        <Box
+          display="block"
+          justifyContent="flex-start"
+        >
+          <EmojiEventsIcon
+            sx={{ color: colors.purpleAccent[600], fontSize: "26px", marginLeft:"30px", marginTop:"30px"}}
           />
+          <Typography 
+            variant="h3" 
+            color={colors.purpleAccent[800]}
+            align="left"
+            margin={30+"px"}
+            marginBottom={0+"px"}
+            marginTop={0+"px"}
+            fontWeight="bold"
+          >
+            35
+          </Typography>
+          <Typography
+            marginLeft={30+"px"}
+            fontSize={16}
+            >
+            Rewards Earned
+          </Typography>
+        </Box>
         </Box>
         <Box
           gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
+          backgroundColor={"#FFA500"}
           display="flex"
-          alignItems="center"
-          justifyContent="center"
+          borderRadius="5px"
         >
-          <StatBox
-            title="1,325,134"
-            subtitle="Traffic Received"
-            progress="0.80"
-            increase="+43%"
-            icon={
-              <TrafficIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
+        <Box
+          display="block"
+          justifyContent="flex-start"
+        >
+          <EmojiEventsIcon
+            sx={{ color: colors.purpleAccent[600], fontSize: "26px", marginLeft:"30px", marginTop:"30px"}}
           />
+          <Typography 
+            variant="h3" 
+            color={colors.purpleAccent[800]}
+            align="left"
+            margin={30+"px"}
+            marginBottom={0+"px"}
+            marginTop={0+"px"}
+            fontWeight="bold"
+          >
+            35
+          </Typography>
+          <Typography
+            marginLeft={30+"px"}
+            fontSize={16}
+            >
+            Rewards Earned
+          </Typography>
+        </Box>
         </Box>
 
         {/* ROW 2 */}
         <Box
-          gridColumn="span 8"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
+          gridColumn="span 6"
+          gridRow="span 3"
+          backgroundColor={"#FFA500"}
+          borderRadius="5px"
+          display="flex"
+          flexDirection="column"
         >
           <Box
             mt="25px"
@@ -141,83 +256,74 @@ const Dashboard = () => {
               <Typography
                 variant="h5"
                 fontWeight="600"
-                color={colors.grey[100]}
+                color={colors.purpleAccent[500]}
               >
-                Revenue Generated
+               Top 5
               </Typography>
               <Typography
                 variant="h3"
                 fontWeight="bold"
-                color={colors.greenAccent[500]}
+                color={colors.purpleAccent[500]}
               >
-                $59,342.32
+                Leaderboard
               </Typography>
             </Box>
             <Box>
               <IconButton>
                 <DownloadOutlinedIcon
-                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                  sx={{ fontSize: "26px", color: colors.purpleAccent[500] }}
                 />
               </IconButton>
             </Box>
           </Box>
-          <Box height="250px" m="-20px 0 0 0">
-            <LineChart isDashboard={true} />
-          </Box>
-        </Box>
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          overflow="auto"
-        >
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            borderBottom={`4px solid ${colors.primary[500]}`}
-            colors={colors.grey[100]}
-            p="15px"
-          >
-            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
-              Recent Transactions
-            </Typography>
-          </Box>
-          {mockTransactions.map((transaction, i) => (
-            <Box
-              key={`${transaction.txId}-${i}`}
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              borderBottom={`4px solid ${colors.primary[500]}`}
-              p="15px"
-            >
-              <Box>
-                <Typography
-                  color={colors.greenAccent[500]}
-                  variant="h5"
-                  fontWeight="600"
-                >
-                  {transaction.txId}
-                </Typography>
-                <Typography color={colors.grey[100]}>
-                  {transaction.user}
-                </Typography>
-              </Box>
-              <Box color={colors.grey[100]}>{transaction.date}</Box>
-              <Box
-                backgroundColor={colors.greenAccent[500]}
-                p="5px 10px"
-                borderRadius="4px"
-              >
-                ${transaction.cost}
-              </Box>
-            </Box>
+          <Box height="250px" p="15px" flexGrow={1}>
+          <TableContainer component={Paper}>
+      <Table sx={{ minWidth: "250px" }} aria-label="customized table">
+        <TableHead sx={{fontSize: 24}}>
+          <TableRow sx={{backgroundColor: colors.purpleAccent[700]}}>
+            <StyledTableCell fontSize="20">Position</StyledTableCell>
+            <StyledTableCell align="left">Name</StyledTableCell>
+            <StyledTableCell align="left">Time</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="left">{row.calories}</StyledTableCell>
+              <StyledTableCell align="left">{row.fat}</StyledTableCell>
+            </StyledTableRow>
           ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+          </Box>
         </Box>
 
-        {/* ROW 3 */}
         <Box
+          gridColumn="span 6"
+          gridRow="span 3"
+          backgroundColor="transparent"
+          
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+          </Box>
+          <Box>
+              <img src={Track} height="400px" width="550px" m="-15px 0 0 0"></img>
+          </Box>       
+        </Box>
+        
+
+        {/* ROW 3 */}
+        {/* <Box
           gridColumn="span 4"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -242,8 +348,8 @@ const Dashboard = () => {
             </Typography>
             <Typography>Includes extra misc expenditures and costs</Typography>
           </Box>
-        </Box>
-        <Box
+        </Box> */}
+        {/* <Box
           gridColumn="span 4"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -264,7 +370,7 @@ const Dashboard = () => {
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
           padding="30px"
-        ></Box>
+        ></Box> */}
       </Box>
     </Box>
   );
