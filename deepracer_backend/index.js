a5473726f6fe0a3d75a5cb8682af97fb4812753e
domlindros <s3900388@student.rmit.edu.au>
2023-09-04 00:06:41 +1000

    firebase connection and some initial endpoints created

--- deepracer_backend/index.js
+++ deepracer_backend/index.js
@@ -4,7 +4,43 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//Simple request time logger
+// require firebase-admin module
+const admin = require("firebase-admin");
+
+// require service account key
+const serviceAccount = require("./deepracer-52ec7-firebase-adminsdk-z1mqa-0560f04062.json");
+
+// initialise database connection
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://console.firebase.google.com/u/0/project/deepracer-52ec7/firestore"
+});
+const db = admin.firestore();
+
+// get data from teams collection in database
+const getDataFromFirebase = async (modelName) => {
+    try {
+        const snapshot = await db.collection(modelName).get();
+        // create dict
+        const data = [];
+
+        // push each doc from collection to list
+        snapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...doc.data() });
+        });
+
+        return data;
+
+    } 
+    // catch error
+    catch (error) {
+        console.error("Error getting data from Firebase: ", error);
+        throw error;
+    }
+};
+
+
+// log time of requests to backend
 app.use(function(req, res, next){
     console.log("A new request received at " + Date.now());
     //This function call is very important. It tells that more processing is
@@ -14,10 +50,34 @@ app.use(function(req, res, next){
 });
 
 
+// test endpoint
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
+app.get("/teams", (req, res) =>{
+    // run function to get data from database
+    getDataFromFirebase("teams")
+    // return result (TODO: testing)
+    .then((result) => {
+        res.json(result)
+    })
+    .catch((error) => {
+        console.error("Error: ", error);
+    });
+});
+
+app.get("/models", (req, res) =>{
+    // run function to get data from database
+    getDataFromFirebase("models")
+    // return result (TODO: testing)
+    .then((result) => {
+        res.json(result)
+    })
+    .catch((error) => {
+        console.error("Error: ", error);
+    });
+});
 
 app.get('*', (req, res) => {
     res.send('Sorry, this is an invalid URL.');
