bcac3dd64bf11bf8639bdb63e57628770e8736e3
Rishi <rishipatel3012@gmail.com>
2023-09-19 21:16:17 +1000

    changed the root route from dashboard to welcome page

--- deepracer_dashboard/src/App.js
+++ deepracer_dashboard/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
@@ -8,28 +8,50 @@ import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
+import LandingPage from "./scenes/landingPage";
+import Register from "./scenes/register";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { colourModeContext, useMode } from "./theme";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+  const getCurrentRoute = () => {
+    return window.location.pathname;
+  };
+  useEffect(() => {
+    const currentRoute = getCurrentRoute();
+    if (currentRoute === '/') {
+      setIsSidebar(false);
+    } else if (currentRoute === '/register'){
+      setIsSidebar(false);
+    } 
+    else if (currentRoute === '/login') {
+      setIsSidebar(false);
+    }
+    else {
+      setIsSidebar(true);
+    }
+  }, [isSidebar]);
+
 
   return (
     <colourModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar isSidebar={isSidebar} />
+          {getCurrentRoute() !== '/' && <Sidebar isSidebar={isSidebar} />}
           <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
+            {getCurrentRoute() !== '/' && <Topbar setIsSidebar={setIsSidebar} />}
             <Routes>
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/leaderboard" element={<Leaderboard />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/regsiter" element={<Register />} />
             </Routes>
           </main>
         </div>
