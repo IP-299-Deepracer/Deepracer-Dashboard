8daba819cb02c2eb832b2a5a08b196dc7069be32
Rishi <rishipatel3012@gmail.com>
2023-09-28 15:22:14 +1000

    removed the footer code

--- deepracer_dashboard/src/scenes/landingPage/index.jsx
+++ deepracer_dashboard/src/scenes/landingPage/index.jsx
@@ -35,37 +35,6 @@ const LandingPage = () => {
           </Box>
         </Box>
       </Container>
-      <Paper sx={{marginTop: 'calc(10% + 50px)',
-    width: '100%',
-    position: 'fixed',
-    bottom: 0,
-    width: '100%',
-    backgroundColor: colours.default
-    }} component="footer">
-      <Container maxWidth="lg">
-        {/* <Box
-          sx={{
-            flexGrow: 1,
-            justifyContent: "center",
-            display: "flex",
-            my:1
-          }}
-        >
-        </Box> */}
-
-        <Box
-          sx={{
-            flexGrow: 1,
-            justifyContent: "center",
-            display: "flex",
-            mb: 2,
-          }}
-        >
-          <img src={RMIT} alt="" height={35 + 'px'} width={110 + 'px'} />
-          <img src={AWS} alt="" height={35+ 'px'} width={110+ 'px'}  sx={{marginLeft: 16}}></img>
-        </Box>
-      </Container>
-    </Paper>
     </ThemeProvider>
     
     
