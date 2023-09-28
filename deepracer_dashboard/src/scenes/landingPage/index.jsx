7fb722a9521ba7ddad5af7178a9cf0dafa1e6241
Rishi <rishipatel3012@gmail.com>
2023-09-28 14:28:27 +1000

    Added footer and styled the title and button

--- deepracer_dashboard/src/scenes/landingPage/index.jsx
+++ deepracer_dashboard/src/scenes/landingPage/index.jsx
@@ -4,6 +4,10 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { tokens } from "../../theme";
 import { Route, Routes } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import Paper from '@mui/material/Paper';
+import RMIT from "../../assets/aws.png";
+import AWS from "../../assets/rmit.png"
+
 
 const theme = createTheme();
 const colours = tokens(theme.palette.mode);
@@ -15,30 +19,56 @@ const LandingPage = () => {
   };
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" maxWidth="m">
         <Box sx={{ paddingTop: 30, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-          <Typography component="h1" variant="h3">
-            WELCOME!
+          <Typography component="h1" variant="h3" sx={{fontWeight: 'bold', fontStyle: 'justify', color: colours.orangeAccent[500]}}>
+            WELCOME TO THE DEEPRACER DASH!
           </Typography>
-          <Box sx={{ paddingTop: 2, display: "block", justifyContent: "center", paddingLeft: 4 }}>
-            <Button 
-            variant="contained" 
-            sx={{ margin: 2, height: 50, width: 300, backgroundColor: "#f79400", ":hover": { backgroundColor:  colours.purpleAccent[400] }}} 
-
-            >
-              Start Event
-            </Button>
+          <Box sx={{ paddingTop: 10, display: "flex", justifyContent: "center", alignItems: "center"}}>
             <Button
             onClick={
               handleJoinEvent
             }
             variant="contained" sx={{ margin: 2, height: 50, width: 300, backgroundColor: "#f79400", ":hover": { backgroundColor:  colours.purpleAccent[400] }}}>
-              Join Event
+              Let's Race!
             </Button>
           </Box>
         </Box>
       </Container>
+      <Paper sx={{marginTop: 'calc(10% + 50px)',
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: colours.default
+    }} component="footer">
+      <Container maxWidth="lg">
+        {/* <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            my:1
+          }}
+        >
+        </Box> */}
+
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            mb: 2,
+          }}
+        >
+          <img src={RMIT} alt="" height={35 + 'px'} width={110 + 'px'} />
+          <img src={AWS} alt="" height={35+ 'px'} width={110+ 'px'}  sx={{marginLeft: 16}}></img>
+        </Box>
+      </Container>
+    </Paper>
     </ThemeProvider>
+    
+    
   );
 };
 
