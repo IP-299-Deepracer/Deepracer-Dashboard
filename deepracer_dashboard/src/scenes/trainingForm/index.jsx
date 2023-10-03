2772f5cc69390c89cac7db1a25460cf4395ae7f5
Rishi <rishipatel3012@gmail.com>
2023-10-03 21:46:26 +1100

    created the form and styled the form

--- deepracer_dashboard/src/scenes/trainingForm/index.jsx
+++ deepracer_dashboard/src/scenes/trainingForm/index.jsx
@@ -1,19 +1,39 @@
 import Header from "../../components/Header";
 import { Box,  Button, TextField } from "@mui/material";
 import React from 'react';
+import Grid from '@mui/material/Grid';
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import "../trainingForm/training.css"
 
 
 const TrainingForm = () => {
 
     return(
-        <Box m="20px">
+        <Box m="20px" >
             <Header title="Training Data Upload" subtitle="Upload training logs" />
-            <form sx={{display: 'flex', flexDirection:'column', alignItems: 'center'}}>
-            <TextField id="outlined-basic" label="Model Name" variant="outlined" />
-            </form>
+            <Box marginTop="10%">
+                <form>
+                    <Grid container spacing={2} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                        <Grid item xs={8}>
+                            <TextField label="Model Name" variant="filled" className="text-field"/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <input type="file" id="file1" name="file1" accept=".csv" label="Upload" className="input-field"
+                            />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <input type="file" id="file2" name="file2" accept=".csv" className="input-field"/>
+                        </Grid>
+                        <Grid item xs={8}>
+                        <Button type="submit" color="secondary" variant="contained">
+                            Create New User
+                        </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Box>
         </Box>
     );
 };
