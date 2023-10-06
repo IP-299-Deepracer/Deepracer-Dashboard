99da1df14d658bfb314e1e71a0e4abdad274c532
Rishi <rishipatel3012@gmail.com>
2023-10-06 21:54:07 +1100

    removed an import element

--- deepracer_dashboard/src/scenes/RaceDayForm/index.jsx
+++ deepracer_dashboard/src/scenes/RaceDayForm/index.jsx
@@ -1,10 +1,7 @@
 import Header from "../../components/Header";
-import { Box,  Button, TextField, Typography } from "@mui/material";
+import { Box,  Button, Typography } from "@mui/material";
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import { Formik } from "formik";
-import * as yup from "yup";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import "../trainingForm/training.css"
 
 
